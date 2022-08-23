fa43fc41f51d1aae50cc6dd7106578a9c84bdcb0
Louis <huynguyennbk@gmail.com>
2022-08-23 21:09:18 +0700

    feat: valid token before request media rpc

--- app/rpc/concerns/authorize.rb
+++ app/rpc/concerns/authorize.rb
@@ -1,7 +1,31 @@
+class AuthMetaInterceptor < Gruf::Interceptors::ClientInterceptor
+  def initialize(token_key)
+    @token_key = token_key
+    super
+  end
+
+  def call(request_context:)
+    request_context.metadata['authorization'] = @token_key
+    yield
+  end
+end
+
 module Concerns
   module Authorize
     def authorize_request!
-      # TODO: authorize with auth service
+      client = ::Gruf::Client.new(
+        service: ::Auth::JwtService,
+        client_options: {
+          interceptors: [AuthMetaInterceptor.new(request.metadata['authorization'])]
+        }
+      )
+
+      response = client.call(:ValidToken)
+
+      raise Gruf::Client::Error, 'Token is invalid' unless response.message.valid
+    rescue Gruf::Client::Error => e
+      set_debug_info(e.message, e.backtrace[0..4])
+      fail!(:unauthenticated, :unauthenticated, "ERROR: #{e.message}")
     end
   end
 end
--- config/initializers/gruf.rb
+++ config/initializers/gruf.rb
@@ -1,9 +1,27 @@
 require "gruf"
 
 Gruf.configure do |c|
-  c.server_binding_url = '0.0.0.0:9080'
   c.grpc_logger = Logger.new(STDOUT)
   c.logger = Logger.new(STDOUT)
+
+  # Set up rpc server host
+  c.server_binding_url = if Rails.env.production?
+    server_port = Rails.application.credentials.rpc.try(:[], 'server_port')
+
+    "localhost:#{server_port}"
+  else
+    "localhost:#{ENV['RPC_SERVER_PORT']}"
+  end
+
+  # Set up rpc client host
+  c.default_client_host = if Rails.env.production?
+    client_host = Rails.application.credentials.rpc.try(:[], 'client_host')
+    client_port = Rails.application.credentials.rpc.try(:[], 'client_port')
+
+    "#{client_host}:#{client_port}"
+  else
+    "localhost:#{ENV['RPC_CLIENT_PORT']}"
+  end
 
   Dir.glob(Rails.root.join("app/proto/*_pb.rb")).each do |file|
     require file
